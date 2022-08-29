02d9585438c122e7308beb09cdef4d362e240261
Rajpal Singh <rajpalsingh@Rajpals-MacBook-Pro.local>
2022-08-29 12:52:55 +0100

    app setup

--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ import dbConfig from './config/database';
     // Connect to the database
     const db = new Database(environment.nodeEnv, dbConfig);
     await db.connect();
+
+    // Import App and set up the server
+    const App = require('./app').default;
+    const app = new App();
+    app.listen();
   } catch (e) {
     console.error('Something went wrong:\n', e.stack);
   }
